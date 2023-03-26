08b07aa8e9737aa5b5a8a976b09c77f3cae9abfb
= <=>
2023-03-26 14:14:09 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+} from "react-router-dom";
+import Unit1OsunRoutes from "./routes/authroutes/Unit1OsunRoutes";
+import React from "react";
+import axios from "axios";
+import Home from "./pages/Home";
+import Login from "./components/Auth/Login";
+import Frontdesk from "./components/Unit/unit1Osun/Frontdesk";
+import Unit2OsunRoutes from "./routes/authroutes/Unit2OsunRoutes";
+import MoreInfo from "./components/MoreInfo/MoreInfo";
+
+
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("auth_token");
+  config.headers.Authorization = token ? `Bearer  ${token}` : "";
+  return config;
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login">
+            {localStorage.getItem("auth_token") ? (
+              <Redirect to={`/`} />
+            ) : (
+              <Login />
+            )}
+          </Route>
+       
+          <Unit1OsunRoutes path="/unit1Osun" name="Unit1Osun" />
+          <Unit2OsunRoutes path="/unit2Osun" name="Unit2Osun"/>
+        
+        </Switch>
+      </Router>
+
     </div>
   );
 }
